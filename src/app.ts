68f0d2095ad49037dfc892b72445f9216de4ac6e
jahidhowlader <jahidhowlader.info@gmail.com>
2025-03-16 13:42:59 +0600

    ready backend server template

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 
 // parsers
-app.use(express.json())
-app.use(cors())
-
-// var test = 'jahid howlader'
-// console.log({
-//     test
-// });
-
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-export default app
+export default app;
