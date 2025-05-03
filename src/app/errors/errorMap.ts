f2c41d9b67d6e965b0bc6b26b969ceca752a0c32
jahidhowlader <jahidhowlader.info@gmail.com>
2025-05-03 22:35:07 +0600

    remove error map

--- src/app/errors/errorMap.ts
+++
@@ -1,9 +0,0 @@
-export const errorMap: Record<number, string> = {
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    403: 'Forbidden',
-    404: 'Not Found',
-    409: 'Conflict',
-    422: 'Unprocessable Entity',
-    500: 'Internal Server Error',
-};
